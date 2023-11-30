f97d88592cd679a58f1ac8c6ee9345eb717d240a
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-11-29 21:50:14 -0700

    Remove Vehicle Dto, not needed

--- Dtos/Vehicle/VehicleDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ParkingTrackerAPI.Dtos.Vehicle
-{
-    public class VehicleDto
-    {
-        public string PlateNumber { get; set; } = null!;
-
-        public string PlateRegion { get; set; } = null!;
-    }
-}
